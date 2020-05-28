dc63dee5e592662d861b02f47d2842369b7b7e79
Lorna Kamau <kamaulorna@gmail.com>
2020-05-28 17:46:35 +0300

    create config classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from .config import DevConfig
 
 app = Flask(__name__) #instance of app
+
+app.config.from_object(DevConfig) #setting up configuration
 
 from app import views
