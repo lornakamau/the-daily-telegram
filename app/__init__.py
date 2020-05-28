408c755047d4d8abedb5822e757e4a7f9c3ae83d
Lorna Kamau <kamaulorna@gmail.com>
2020-05-28 23:09:09 +0300

    create base.html template and add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__,instance_relative_config= True) #instance of app
 
 #setting up configuration
 app.config.from_object(DevConfig) 
 app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
