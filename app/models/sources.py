6a50b625b1e64ed1ac14059da5a6ea867e44be72
Lorna Kamau <kamaulorna@gmail.com>
2020-05-28 18:59:17 +0300

    initialize the sources class

---
+++ app/models/sources.py
@@ -0,0 +1,10 @@
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+    def __init__(self,name,description,url,category,language):
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
--- app/sources_test.py
+++ app/sources_test.py
@@ -14,8 +14,17 @@ class SourcesTest(unittest.TestCase):
         self.new_source = Sources('ABC News', 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.', 'https://abcnews.go.com', 'general', 'en')
 
     def test_instance(self):
+        '''
+        Test case to test if the object is an instance of the class
+        '''
         self.assertTrue(isinstance(self.new_source, Sources))
+
+    def test_initialization(self):
+        '''
+        Test initialization test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_source.name, 'ABC News' )
+        self.assertEqual(self.new_source.language, 'en' )
 
 if __name__ == '__main__':
     unittest.main()
-    
